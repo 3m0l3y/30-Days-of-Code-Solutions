a8d523c70e5df1be3c05f38cabeefe72e9439ca9
3m0l3y <emily@aggdata.com>
2016-08-17 15:55:52 -0700

    day 10 incomplete

--- 30-Days-of-Code-Solutions/Day 10.java
+++ 30-Days-of-Code-Solutions/Day 10.java
@@ -1,4 +1,3 @@
-// ***THIS TEXT ISNT WORKING YET***
 // Given a number, convert to binary, and print out how many consecutive 1's occur.
 // Rather than using built in methods, I'm trying to do it manual style.
 import java.io.*;
@@ -14,35 +13,22 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int myInt = in.nextInt();
-        int moded = 0;
-        int answer = 0;
         int consecutiveOnes = 0;
-        int arrayPosition = 0;
+        int mod = 0;
+        int answer = 0;
         int array[] = new int[8];
-        // For each bit in an array of bits
+        int arrayPosition = 0;
+        // for each bit in an array of bytes
         for(int i = 0; i < 8; i++){
-            moded = myInt % 2;
+            mod = myInt % 2;
             answer = myInt / 2;
-            if((moded == 1) && (answer >= 1)){
-                array[i] = 1;
-                arrayPosition = i + 1;
-                array[arrayPosition] = 1;
-                consecutiveOnes++;
-                break;
-            }
-            else if((moded == 1) && (answer >= 2)){
-                array[i] = 1;
-                arrayPosition = i - 1;
-                if((arrayPosition > -1) && (array[arrayPosition] == 1)){
-                    consecutiveOnes++;
-                }
-            }
-            else {
-                array[i] = 0;            
-           }
+            array[i] = mod;
             myInt = answer;
-        }
-       System.out.println(Arrays.toString(array));
-       System.out.println("Number of consecutiveOnes: " + consecutiveOnes );
+   
+            System.out.println("mod = " + mod );
+            System.out.println("answer = " + answer); 
+        }   
+        System.out.println(Arrays.toString(array));
+        System.out.println("consecutiveOnes = " + consecutiveOnes);
     }
 }
