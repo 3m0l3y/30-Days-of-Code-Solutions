68a8ed5fb3adf2194c3acbb5e56f05234d8dac07
3m0l3y <emily@aggdata.com>
2016-08-17 16:11:59 -0700

    day 10

--- 30-Days-of-Code-Solutions/Day 10.java
+++ 30-Days-of-Code-Solutions/Day 10.java
@@ -1,5 +1,45 @@
-// Given a number, convert to binary, and print out how many consecutive 1's occur.
-// Rather than using built in methods, I'm trying to do it manual style.
+// Given a number, convert to binary, and print out the number of consecutive ones.
+// This script was pulled from the Discussion Board because the solutions proposed by this exercise are incorrect.
+// The example given says input of 5 should output 1, but there are no consecutive 1's in the binary representation of 5.
+// Based on the assumption that consecutive 1's means a number 1 immediately followed by a number 1.
+// See my commented script below for an accurate representation of binary when given a number (n);
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Arrays;
+
+public class Solution {
+    
+    
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int rem=0,s=0,t=0;
+
+
+    while(n>0)
+        {
+        rem=n%2;
+        n=n/2;
+        if(rem==1)
+         {   s++;
+           if(s>=t)
+
+            t=s;
+
+        }
+        else{
+
+            s=0;
+        }   
+    }
+
+    System.out.println(t);
+}
+}
+/*
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -24,11 +64,10 @@ public class Solution {
             answer = myInt / 2;
             array[i] = mod;
             myInt = answer;
-   
             System.out.println("mod = " + mod );
             System.out.println("answer = " + answer); 
         }   
         System.out.println(Arrays.toString(array));
-        System.out.println("consecutiveOnes = " + consecutiveOnes);
     }
 }
+*/
